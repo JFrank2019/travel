e44bdd87323f86653ce804c8691804f5d0fdfbc6
JFrank2019 <jfrank2019@163.com>
2019-10-21 08:22:48 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+// 引入fastclick库
+import fastClick from 'fastclick'
+fastClick.attach(document.body)
+
+// 重置样式
+import './assets/styles/reset.css'
+// 解决1px边框问题
+import './assets/styles/border.css'
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
