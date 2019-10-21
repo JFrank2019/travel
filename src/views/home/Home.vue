221fd52d627d7231f3fac451e5a9f64ff41acd52
JFrank2019 <jfrank2019@163.com>
2019-10-21 17:15:06 +0800

    finish list page

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -14,6 +14,7 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -25,15 +26,21 @@ export default {
   },
   data() {
     return {
+      lastCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo() {
-      this.$http.get('/mock/index.json').then(res => this.getHomeInfoSucc(res))
+      this.$http
+        .get('/mock/index.json?city=' + this.city)
+        .then(res => this.getHomeInfoSucc(res))
     },
     getHomeInfoSucc(res) {
       res = res.data
@@ -47,7 +54,14 @@ export default {
     }
   },
   mounted() {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated() {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
